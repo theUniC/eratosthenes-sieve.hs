48913fa1b42e4b1e8a5bcc6393e2cea70443c4bf
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-09-10 19:37:20 +0100

    3rd step. Primes up to 5

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -2,4 +2,5 @@ module EratosthenesSieve where
 
 primesUpTo :: Integer -> [Integer]
 primesUpTo 3 = [2, 3]
+primesUpTo 5 = [2, 3, 5]
 primesUpTo n = [n]
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,3 +7,4 @@ main = hspec $ do
     it "should return a list with all the prime numbers up to a given number" $ do
       primesUpTo 2 `shouldBe` [2]
       primesUpTo 3 `shouldBe` [2, 3]
+      primesUpTo 5 `shouldBe` [2, 3, 5]
