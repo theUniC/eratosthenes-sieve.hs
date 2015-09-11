17bf1db50595a7689afee06ec25dd8c3aab6b683
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-09-11 10:38:46 +0100

    Refactor. Identify the case for multiples of 3

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -11,7 +11,10 @@ primesUpTo' :: (Eq a, Integral a) => a -> [a]
 primesUpTo' 11 = reverse [2, 3, 5, 7, 11]
 primesUpTo' n
   | n > 2 && n `mod` 2 == 0 = primesUpTo (n - 1)
+  | n > 3 && n `mod` 3 == 0 = primesUpTo (n - 1)
   | n > 2 && not (n `mod` 2 == 0) = n : primesUpTo (n - 1)
+  | n > 3 && not (n `mod` 3 == 0) = n : primesUpTo (n - 1)
   | n == 2 = 2 : primesUpTo (n - 1)
+  | n == 3 = 3 : primesUpTo (n - 1)
   | otherwise = []
 
