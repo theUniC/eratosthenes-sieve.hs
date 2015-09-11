7494b2087744c132ac345b1c7059753645041da3
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-09-11 10:39:12 +0100

    Refactor. Remove unnecesary comments

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -8,7 +8,6 @@ primesUpTo :: (Eq a, Integral a) => a -> [a]
 primesUpTo = reverse . primesUpTo'
 
 primesUpTo' :: (Eq a, Integral a) => a -> [a]
-primesUpTo' 11 = reverse [2, 3, 5, 7, 11]
 primesUpTo' n
   | n > 2 && n `mod` 2 == 0 = primesUpTo (n - 1)
   | n > 3 && n `mod` 3 == 0 = primesUpTo (n - 1)
