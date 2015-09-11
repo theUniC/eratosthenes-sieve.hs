4cce3d9f0fb5b2d7d3779ce986e86e9753df5b6a
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-09-11 10:24:57 +0100

    Refactor. Remove duplicates of 2

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -1,7 +1,19 @@
-module EratosthenesSieve where
+module EratosthenesSieve
+  (
+    primesUpTo
+  )
+where
 
-primesUpTo :: Integer -> [Integer]
-primesUpTo 3 = [2, 3]
-primesUpTo 5 = [2, 3, 5]
-primesUpTo 7 = [2, 3, 5, 7]
-primesUpTo n = [n]
+primesUpTo :: (Eq a, Integral a) => a -> [a]
+-- primesUpTo 7 = [2, 3, 5, 7]
+-- primesUpTo 5 = [2, 3, 5]
+-- primesUpTo 3 = [2, 3]
+primesUpTo = reverse . primesUpTo'
+
+primesUpTo' :: (Eq a, Integral a) => a -> [a]
+primesUpTo' n
+  | n > 2 && n `mod` 2 == 0 = primesUpTo (n - 1)
+  | n > 2 && not (n `mod` 2 == 0) = n : primesUpTo (n - 1)
+  | n == 2 = 2 : primesUpTo (n - 1)
+  | otherwise = []
+
