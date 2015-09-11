65763d79b5bdd2c1b762c1be22b6e2e3557aec97
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-09-11 21:38:08 +0100

    Refactor. Defining sieve function as an internal function of primesUpTo

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -6,7 +6,7 @@ where
 
 primesUpTo :: (Eq a, Integral a) => a -> [a]
 primesUpTo n = sieve [2 .. n]
+  where
+    sieve (x:xs) = x : sieve [p | p <- xs, p `mod` x > 0]
+    sieve []     = []
 
-sieve :: (Eq a, Integral a) => [a] -> [a]
-sieve (x:xs) = x : sieve [p | p <- xs, p `mod` x > 0]
-sieve [] = []
