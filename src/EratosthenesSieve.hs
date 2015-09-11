ba0ef91cae63dd3878e77afb64429c4511f8032a
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-09-11 19:07:01 +0100

    Refactor. Cleaning up

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -5,18 +5,7 @@ module EratosthenesSieve
 where
 
 primesUpTo :: (Eq a, Integral a) => a -> [a]
-primesUpTo = primesUpTo'
-
-primesUpTo' :: (Eq a, Integral a) => a -> [a]
--- primesUpTo' n
---   | n > 2 && n `mod` 2 == 0 = primesUpTo (n - 1)
---   | n > 3 && n `mod` 3 == 0 = primesUpTo (n - 1)
---   | n > 2 && not (n `mod` 2 == 0) = n : primesUpTo (n - 1)
---   | n > 3 && not (n `mod` 3 == 0) = n : primesUpTo (n - 1)
---   | n == 2 = 2 : primesUpTo (n - 1)
---   | n == 3 = 3 : primesUpTo (n - 1)
---   | otherwise = []
-primesUpTo' n = sieve 3 $ sieve 2 [2 .. n]
+primesUpTo n = sieve 3 $ sieve 2 [2 .. n]
 
 sieve :: (Eq a, Integral a) => a -> [a] -> [a]
 sieve _ [] = []
