7cc74c51e43d567805a7ede80d168d5e1437fdd7
Christian Soronellas <christian.soronellas@atrapalo.com>
2015-09-22 22:09:47 +0100

    Sugar-free monadic version of Eratosthenes Sieve Kata

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -4,9 +4,10 @@ module EratosthenesSieve
   )
 where
 
+import Control.Monad (guard)
+
 primesUpTo :: Integer -> [Integer]
 primesUpTo n = sieve [2 .. n]
   where
-    sieve (x:xs) = x : sieve [p | p <- xs, p `mod` x > 0]
     sieve []     = []
-
+    sieve (x:xs) = x : sieve (xs >>= \p -> guard (p `mod` x > 0) >> return p)
