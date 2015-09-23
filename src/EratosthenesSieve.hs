f850817325ae45d3ec46c3779f43d543a85fb7ef
tλeUniC <theunic@gmail.com>
2015-09-23 11:15:18 +0200

    Update EratosthenesSieve.hs

--- src/EratosthenesSieve.hs
+++ src/EratosthenesSieve.hs
@@ -11,3 +11,8 @@ primesUpTo n = sieve [2 .. n]
   where
     sieve []     = []
     sieve (x:xs) = x : sieve (xs >>= \p -> guard (p `mod` x > 0) >> return p)
+    -- with syntactic sugar
+    -- sieve (x:xs) = x : (sieve $ do
+    --  p <- xs
+    --  guard (p `mod` x > 0)
+    --  return p)
